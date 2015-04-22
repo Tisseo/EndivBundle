9e8f13e990e234ffd78d7add21b6ca29409951ac
Pierre-Yves CLAITTE <pierre.cl@gmail.com>
2015-04-22 17:07:55 +0200

    schemas (in progress)

--- Services/LineManager.php
+++ Services/LineManager.php
@@ -10,11 +10,13 @@ class LineManager extends SortManager
     private $om = null;
     private $repository = null;
 
+
     public function __construct(ObjectManager $om)
     {
         $this->om = $om;
         $this->repository = $om->getRepository('TisseoEndivBundle:Line');
     }
+
 
     public function findAll()
     {
@@ -25,6 +27,7 @@ class LineManager extends SortManager
     {
         return empty($lineId) ? null : $this->repository->find($lineId);
     }
+
 
     public function findExistingNumber($number, $id)
     {
@@ -37,6 +40,7 @@ class LineManager extends SortManager
         return $query->getResult();
     }
 
+
     public function findAllLinesByPriority()
     {
         $query = $this->repository->createQueryBuilder('l')
@@ -45,6 +49,27 @@ class LineManager extends SortManager
 
         return $this->sortLinesByNumber($query->getResult());
     }
+
+
+    public function findAllLinesWithSchematic($splitByPhysicalMode = false) {
+        $query = $this->repository->createQueryBuilder('l')
+            ->leftJoin('l.schematics', 'sc')
+            ->getQuery();
+
+        $result = $this->sortLinesByNumber($query->getResult());
+
+        if ($splitByPhysicalMode) {
+            $query = $this->om->createQuery(
+                "SELECT p.name FROM Tisseo\EndivBundle\Entity\PhysicalMode p"
+            );
+            $physicalModes = $query->getResult();
+
+            $result = $this->splitByPhysicalMode($result, $physicalModes);
+        }
+
+        return $result;
+    }
+
 
     public function save(Line $line)
     {
