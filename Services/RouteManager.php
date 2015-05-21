83a870ff5036735a78db4c75d1d75441c3887df4
Rodolphe DUVAL <rdldvl@gmail.com>
2015-05-21 08:53:24 +0200

    boa route stops first functional implementation (not full)

--- Services/RouteManager.php
+++ Services/RouteManager.php
@@ -10,12 +10,16 @@ namespace Tisseo\EndivBundle\Services;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use Doctrine\DBAL\Query\QueryBuilder;
-use Tisseo\EndivBundle\Entity\Route;
 
+use Tisseo\EndivBundle\Entity\Route;
+use Tisseo\EndivBundle\Entity\RouteStop;
+use Tisseo\EndivBundle\Entity\Trip;
+use Tisseo\EndivBundle\Entity\StopTime;
+
+/*
 use Tisseo\EndivBundle\Entity\LineVersion;
-use Tisseo\EndivBundle\Services\TripManager;
+*/
 
 class RouteManager extends SortManager 
 {
@@ -33,6 +37,122 @@ class RouteManager extends SortManager
         return $this->repository->findAll();
     }
 
+    public function findById($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    public function save(Route $route)
+    {
+        $this->om->persist($route);
+        $this->om->flush();
+    }
+
+    public function saveRouteStopsAndServices(Route $route, $route_stops, $services)
+    {
+        $rank = 1;
+        $doctrine_route_stops = array();
+        $route_stop_ids = array();
+        foreach ($route_stops as $route_stop) {
+            if( isset($route_stop['id']) ) {
+                //update existing route stop
+                $route_stop_id = $route_stop['id'];
+                $route_stop_ids[] = $route_stop_id;
+                $filtered_collection = $route->getRouteStops()->filter( function($rs) use ($route_stop_id) {
+                    return $rs->getId() == $route_stop_id;
+                });
+                $doctrine_route = $filtered_collection->first();
+            } else {
+                //new route stop
+                $doctrine_route = new RouteStop();
+                $waypoint = $this->om
+                            ->getRepository('TisseoEndivBundle:Waypoint')
+                            ->find($route_stop['waypoint_id']);
+                $doctrine_route->setWaypoint($waypoint);
+                $route->addRouteStops($doctrine_route);
+            }
+            $doctrine_route->setRank($rank);
+            $doctrine_route->setDropOff( isset($route_stop['dropOff']) );
+            $doctrine_route->setPickup( isset($route_stop['pickUp']) );
+            $doctrine_route->setScheduledStop( isset($route_stop['scheduled']) );
+            $doctrine_route->setInternalService( ( $route->getWay() == 'Zonal' && isset($route_stop['internal']) ) );
+            $this->om->persist($doctrine_route);
+            $doctrine_route_stops[] = $doctrine_route;
+
+            $rank += 1;
+        }
+
+        //deleted route stops case
+        foreach ($route->getRouteStops() as $rs) {
+            if( !in_array($rs->getId(), $route_stop_ids) ) {
+                $route->removeRouteStops($rs);
+                $this->om->remove($rs);
+            }
+        }
+
+        $trip_ids = array();
+        foreach ($services as $service) {
+            if( isset($service['id']) ) {
+                //update existing service
+                $trip_id = $service['id'];
+                $trip_ids[] = $trip_id;
+                unset($service['id']);
+                $filtered_route_trips = $route->getTrips()->filter( function($t) use ($trip_id) {
+                    return $t->getId() == $trip_id;
+                });
+                $doctrine_trip = $filtered_route_trips->first();
+            } else {
+                //new service
+                $doctrine_trip = new Trip();
+                $doctrine_trip->setRoute($route);
+                $route->addTrip($doctrine_trip);
+            }
+
+            $trip_name = $service['name'];
+            unset($service['name']);
+            $doctrine_trip->setName($trip_name);
+            $doctrine_trip->setIsPattern(true);
+
+            $stop_time_index = 0;
+            foreach ($service as $stop_time) {
+
+                if( isset($stop_time['stop_time_id']) ) {
+                    //update stop time
+                    $stop_time_id = $stop_time['stop_time_id'];
+                    $filtered_trip_stop_time = $doctrine_trip->getStopTimes()->filter( function($st) use ($stop_time_id) {
+                        return $st->getId() == $stop_time_id;
+                    });
+                    $doctrine_stop_time = $filtered_trip_stop_time->first();
+                } else {
+                    //new stop time
+                    $doctrine_stop_time = new StopTime();
+                    $doctrine_trip->addStopTime($doctrine_stop_time);
+                }
+
+                $date = \DateTime::createFromFormat("Y-m-d H:i:s", "1970-01-01 ".$stop_time['time'].":00");
+                $doctrine_stop_time->setRouteStop($doctrine_route_stops[$stop_time_index]);
+                $doctrine_stop_time->setTrip($doctrine_trip);
+                $doctrine_stop_time->setArrivalTime($date->getTimestamp());
+                $doctrine_stop_time->setDepartureTime($date->getTimestamp());
+                $this->om->persist($doctrine_stop_time);
+
+                $stop_time_index += 1;
+            }
+
+            $this->om->persist($doctrine_trip);
+        }
+
+        //deleted trips case
+        foreach ($route->getTrips() as $t) {
+            if( !in_array($t->getId(), $trip_ids) ) {
+                $route->removeTrip($t);
+                $this->om->remove($t);
+            }
+        }
+
+        $this->save($route);
+    }
+
     public function getRoutesByLine($lineVersionId)
     {
         $query = $this->om->createQuery("
@@ -43,7 +163,6 @@ class RouteManager extends SortManager
         ")
         ->setParameter("id", $lineVersionId);
 
-        
         return $query->getResult();
     }
 
@@ -93,7 +212,7 @@ class RouteManager extends SortManager
 
 
 
-
+/*
     public function findAllByLine($id)
     {
         $query = $this->repository->createQueryBuilder('r')
@@ -105,10 +224,6 @@ class RouteManager extends SortManager
         return $query->getResult();
     }
 
-    public function findById($id)
-    {
-        return $this->repository->find($id);
-    }
 
 
 
@@ -149,11 +264,6 @@ class RouteManager extends SortManager
     }
 
 
-    public function save(Route $route)
-    {
-        $this->om->persist($route);
-        $this->om->flush();
-    }
 
     public function removeRoute(Route $route)
     {
@@ -197,5 +307,5 @@ class RouteManager extends SortManager
                     return true;
                 }
     }
-
+*/
 }
