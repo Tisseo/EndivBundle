85dc984ec0bd5f9b27ed1628f26778a4b5e6b121
Vincent Passama <vincent.passama@capgemini.com>
2015-08-19 15:41:36 +0200

    Removing old comments.

--- Entity/CalendarElement.php
+++ Entity/CalendarElement.php
@@ -42,13 +42,7 @@ class CalendarElement
     /**
      * @var \Tisseo\EndivBundle\Entity\Calendar
      */
-    // private $calendar;
-
-    /**
-     * @var \Tisseo\EndivBundle\Entity\Calendar
-     */
     private $includedCalendar;
-
 
     /**
      * Get id
@@ -174,29 +168,6 @@ class CalendarElement
     {
         return $this->interval;
     }
-
-    // /**
-     // * Set calendar
-     // *
-     // * @param \Tisseo\EndivBundle\Entity\Calendar $calendar
-     // * @return CalendarElement
-     // */
-    // public function setCalendar(Calendar $calendar = null)
-    // {
-        // $this->calendar = $calendar;
-
-        // return $this;
-    // }
-
-    // /**
-     // * Get calendar
-     // *
-     // * @return \Tisseo\EndivBundle\Entity\Calendar
-     // */
-    // public function getCalendar()
-    // {
-        // return $this->calendar;
-    // }
 
     /**
      * Set includedCalendar
