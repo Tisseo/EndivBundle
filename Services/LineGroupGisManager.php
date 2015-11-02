8aaf65d5a525f2ce8aad75faf446cafd4852fec4
Vincent Passama <vincent.passama@capgemini.com>
2015-11-02 10:30:31 +0100

    Adding export function for LineGroupGis.

--- Services/LineGroupGisManager.php
+++ Services/LineGroupGisManager.php
@@ -70,4 +70,30 @@ class LineGroupGisManager extends SortManager
         $this->om->remove($lineGroupGis);
         $this->om->flush();
     }
+
+    public function getCsvExport()
+    {
+        $query = $this->om->createQuery("
+            SELECT
+                lgg.name as line_group_gis_name,
+                l.number as line_number,
+                max(s.date) as first_schematic,
+                min(s.date) as last_schematic,
+                lv.depot as line_version_depot
+            FROM Tisseo\EndivBundle\Entity\Schematic s
+            JOIN s.line l
+            JOIN l.lineVersions lv
+            JOIN l.lineGroupGisContents lggc
+            JOIN lggc.lineGroupGis lgg
+            WHERE s.groupGis = true
+            AND s.deprecated = false
+            GROUP BY lgg.name, l.number, lv.depot
+        ");
+
+        $content = $query->getArrayResult();
+        $date = new \Datetime();
+        $filename = 'line_group_gis_'.$date->format('Y-m-d');
+
+        return array($content, $filename);
+    }
 }
