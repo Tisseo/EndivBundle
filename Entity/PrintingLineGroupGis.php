e825d67b5a40bf3863787ca76a4424edf646bdad
Jean Lavat <jean.lavat@capgemini.com>
2016-02-12 14:58:23 +0100

    adding entity and manager for lgg printings export feature

---
+++ Entity/PrintingLineGroupGis.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Tisseo\EndivBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Printing
+ */
+class PrintingLineGroupGis
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $quantity;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * @var \Tisseo\EndivBundle\Entity\LineGroupGis
+     */
+    private $lineGroupGis;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     * @return Printing
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Printing
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Printing
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set lineGroupGis
+     *
+     * @param \Tisseo\EndivBundle\Entity\LineGroupGis $lineGroupGis
+     * @return PrintingLineGroupGis
+     */
+    public function setLineGroupGis(LineGroupGis $lineGroupGis = null)
+    {
+        $this->lineGroupGis = $lineGroupGis;
+
+        return $this;
+    }
+
+    /**
+     * Get lineGroupGis
+     *
+     * @return \Tisseo\EndivBundle\Entity\LineGroupGis
+     */
+    public function getLineGroupGis()
+    {
+        return $this->lineGroupGis;
+    }
+}
