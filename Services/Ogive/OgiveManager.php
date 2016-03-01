a9912e9184256f046ccd2c5db602bc13b692c231
Vincent Passama <astek.vpassama@tisseo.fr>
2016-03-01 10:48:14 +0100

    ogive: add optional serializer available for ogive managers.

--- Services/Ogive/OgiveManager.php
+++ Services/Ogive/OgiveManager.php
@@ -4,11 +4,26 @@ namespace Tisseo\EndivBundle\Services\Ogive;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Tisseo\EndivBundle\Entity\Ogive\OgiveEntity;
+use JMS\Serializer\Serializer;
+use Traversable;
+use Exception;
 
 abstract class OgiveManager
 {
+    /**
+     * @var ObjectManager
+     */
     protected $objectManager = null;
+
+    /**
+     * @var EntityRepository
+     */
     protected $repository = null;
+
+    /**
+     * @var Serializer
+     */
+    private $serializer = null;
 
     /**
      * Constructor
@@ -37,25 +52,29 @@ abstract class OgiveManager
      * Save the entity in database
      * 
      * @param OgiveEntity $entity
+     * @return OgiveEntity
      */
     public function save(OgiveEntity $entity)
     {
         $this->objectManager->persist($entity);
         $this->objectManager->flush();
+        $this->objectManager->refresh($entity);
+
+        return $entity;
     }
 
     /**
      * Remove the entity with specific identifier
      *
      * @param integer $identifier
-     * @throws \Exception if entity not found
+     * @throws Exception if entity not found
      */
     public function remove($identifier)
     {
         $entity = $this->repository->find($identifier);
 
         if (empty($entity)) {
-            throw new \Exception(sprintf('The entity %s was not found', $identifier));
+            throw new Exception(sprintf('The entity %s was not found', $identifier));
         }
 
         $this->objectManager->remove($entity);
@@ -68,6 +87,68 @@ abstract class OgiveManager
     public function delete($identifier)
     {
         $this->remove($identifier);
+    }
+
+    /**
+     * Set the serializer
+     *
+     * @param Serializer $serializer
+     */
+    public function setSerializer(Serializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Get serializer
+     *
+     * @return Serializer
+     * @throws Exception if serializer not instanciated
+     */
+    public function getSerializer()
+    {
+        if ($this->serializer instanceof Serializer) {
+            return $this->serializer;
+        }
+
+        throw new Exception("Serializer not instanciated in this manager");
+    }
+
+    /**
+     * Normalize
+     *
+     * @param mixed $data
+     * @return array
+     */
+    public function normalize($data)
+    {
+        if ($data instanceof OgiveEntity) {
+            $result = $this->doNormalize($data);
+        } else if (is_array($data) || $data instanceof Traversable) {
+            $result = array();
+            foreach ($data as $entity) {
+                if (!($entity instanceof OgiveEntity)) {
+                    throw new Exception("Can't normalize unknown entity type");
+                }
+
+                $result[] = $this->doNormalize($entity);
+            }
+        } else {
+            throw new Exception("Can't normalize unrecognized data type");
+        }
+
+        return $result;
+    }
+
+    /**
+     * Do normalize
+     *
+     * @param OgiveEntity $entity
+     * @return array
+     */
+    private function doNormalize($entity)
+    {
+        return json_decode($this->getSerializer()->serialize($entity, 'json'));
     }
 
     /**
