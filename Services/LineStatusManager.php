89003fbea5fca41d6290ef929aed7b18150b85f3
Jean Lavat <jean.lavat@capgemini.com>
2016-09-09 17:39:49 +0200

    adding entity and manager for line_status table

---
+++ Services/LineStatusManager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tisseo\EndivBundle\Services;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Collections\ArrayCollection;
+use Tisseo\EndivBundle\Entity\LineStatus;
+
+class LineStatusManager
+{
+    private $om = null;
+    private $repository = null;
+
+
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+        $this->repository = $om->getRepository('TisseoEndivBundle:LineStatus');
+    }
+
+
+    public function findAll()
+    {
+        return ($this->repository->findAll());
+    }
+
+    /*
+     * save
+     * @param LineStatus $lineStatus
+     *
+     * Persist and save a LineStatus into database.
+     */
+    public function save(LineStatus $lineStatus)
+    {
+        $this->om->persist($lineStatus);
+        $this->om->flush();
+    }
+}
