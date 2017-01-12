b4650c9e464ce90eebd4efb20ffc29d995a26a52
Vincent Passama <vincent.passama@tisseo.fr>
2017-01-12 10:31:04 +0100

    make network publication function more generic

--- Services/Ogive/MessageManager.php
+++ Services/Ogive/MessageManager.php
@@ -13,24 +13,35 @@ class MessageManager extends OgiveManager
      * @param  array $channels
      * @return array
      */
-    public function findNetworkPublications(array $channels = array())
+    public function findNetworkPublications(array $channels = array(), array $objectTypes = array())
     {
         $queryBuilder = $this->objectManager->createQueryBuilder('m');
         $expr = $queryBuilder->expr();
 
         $queryBuilder
-            ->select('m')
+            ->select('m, o, c')
             ->from('TisseoEndivBundle:Ogive\Message', 'm')
-            ->join('m.object', 'o', 'with', $expr->eq('o.objectType', ':type'))
             ->where($expr->lte('m.startDatetime', 'current_timestamp()'))
-            ->andWhere($expr->gt('m.endDatetime', 'current_timestamp()'))
-            ->setParameter('type', OgiveObject::LINE);
+            ->andWhere($expr->gt('m.endDatetime', 'current_timestamp()'));
 
-        // If no channels provided, no filters added
+        // Filter by channels if requested
         if (count($channels) > 0) {
             $queryBuilder
                 ->join('m.channels', 'c', 'with', $expr->in('c.name', ':names'))
                 ->setParameter('names', $channels);
+        } else {
+            $queryBuilder
+                ->join('m.channels', 'c');
+        }
+
+        // Filter by objects type if requested
+        if (count($objectTypes) > 0) {
+            $queryBuilder
+                ->join('m.object', 'o', 'with', $expr->in('o.objectType', ':types'))
+                ->setParameter('types', $objectTypes);
+        } else {
+            $queryBuilder
+                ->join('m.object', 'o');
         }
 
         return $queryBuilder->getQuery()->getResult();
