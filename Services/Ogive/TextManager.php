45865d0fad202bad35368f9529afb3085c609eae
Vincent Passama <vincent.passama@tisseo.fr>
2017-02-15 13:38:33 +0100

    evol: add some custom managers functions to improve some views

--- Services/Ogive/TextManager.php
+++ Services/Ogive/TextManager.php
@@ -4,4 +4,13 @@ namespace Tisseo\EndivBundle\Services\Ogive;
 
 class TextManager extends OgiveManager
 {
+    /**
+     * Customized findAll
+     *
+     * @return array
+     */
+    public function findAll()
+    {
+        return $this->getRepository()->findBy(array(), array('label' => 'ASC'));
+    }
 }
